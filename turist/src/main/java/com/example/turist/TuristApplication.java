2e1d5004364357a8b1cb1965c4db27200cf98f22
yamirestart2020 <73224784+yamirestart2020@users.noreply.github.com>
2021-11-22 18:36:36 +0100

    Inicijalni commit

---
+++ turist/src/main/java/com/example/turist/TuristApplication.java
@@ -0,0 +1,13 @@
+package com.example.turist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristApplication.class, args);
+	}
+
+}
